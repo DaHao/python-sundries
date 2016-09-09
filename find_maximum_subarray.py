a54da234f5af44cce1f60297344dce5108a8b82e
DaHao <jiash561@gmail.com>
2016-09-09 18:15:28 +0800

    add pdb

--- find_maximum_subarray.py
+++ find_maximum_subarray.py
@@ -1,7 +1,29 @@
+import pdb
 class Result:
     left = 0
     right = 0
     sumValue = 0
+
+def find_maximum_subarray(A):
+    if len(A) == 2:
+        result = Result()
+        result.left = 0
+        result.right = 1
+        result.sumValue = A[0] + A[1]
+        return result
+    else:
+        mid = len(A) // 2
+        leftResult = find_maximum_subarray(A[:mid])
+        rightResult = find_maximum_subarray(A[mid:])
+        crossResult = crossMid(A, mid)
+
+        if leftResult > crossResult and leftResult > rightResult:
+            return leftResult
+        elif rightResult > crossResult and rightResult > leftResult:
+            return rightResult
+        else:
+            return crossResult
+
 
 def crossMid(A, mid):
 
@@ -13,7 +35,7 @@ def crossMid(A, mid):
         if(leftSum > leftMaxSum):
             leftMaxSum = leftSum
             leftMaxIdx = idx
-
+    
     rightSum = 0
     rightMaxSum = -1
     rightMaxIdx = -1
@@ -31,7 +53,8 @@ def crossMid(A, mid):
     return result
 
 a = [-1, 2, 3, 4, -5]
-result = crossMid(a, 2)
+pdb.set_trace()
+result = find_maximum_subarray(a)
 print('left Index = ', result.left)
 print('right Index = ', result.right)
 print('Sum = ', result.sumValue)
