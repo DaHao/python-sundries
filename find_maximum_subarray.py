3b7c608bfc3a33a09ccfd1e6472c64522b2b6271
DaHao <jiash561@gmail.com>
2016-09-10 17:28:08 +0800

    correct version

--- find_maximum_subarray.py
+++ find_maximum_subarray.py
@@ -6,45 +6,46 @@ class Result:
     right = 0
     sumValue = 0
 
-def find_maximum_subarray(A):
-    if len(A) == 1:
+def find_maximum_subarray(A, low, high):
+    if not A : return None
+    if low == high:
         result = Result()
-        result.left = 0
-        result.right = 0
-        result.sumValue = A[0]
+        result.left = low
+        result.right = high
+        result.sumValue = A[low]
         return result
     else:
-        mid = len(A) // 2
-        leftResult = find_maximum_subarray(A[:mid])
-        rightResult = find_maximum_subarray(A[mid:])
-        crossResult = crossMid(A, mid)
+        mid = (low + high - 1) // 2
+        leftResult = find_maximum_subarray(A, low, mid)
+        rightResult = find_maximum_subarray(A, mid+1, high)
+        crossResult = crossMid(A, low, mid, high)
 
-        if leftResult >= crossResult and leftResult >= rightResult:
+        if leftResult.sumValue >= crossResult.sumValue and leftResult.sumValue >= rightResult.sumValue:
             return leftResult
-        elif rightResult >= crossResult and rightResult >= leftResult:
+        elif rightResult.sumValue >= crossResult.sumValue and rightResult.sumValue >= leftResult.sumValue:
             return rightResult
         else:
             return crossResult
 
 
-def crossMid(A, mid):
+def crossMid(A, low, mid, high):
 
     result = Result()
 
     leftSum = -sys.maxsize
     sumValue = 0
-    for idx in range(mid, -1, -1):
+    for idx in range(mid, low-1, -1):
         sumValue += A[idx]
-        if(sumValue > leftSum):
+        if sumValue > leftSum:
             leftSum = sumValue
             result.left = idx
 
     rightSum = -sys.maxsize
     sumValue = 0
-    for idx in range(mid+1, len(A)):
-        rightSum += A[idx]
-        if(rightSum > rightSum):
-            rightSum = rightSum
+    for idx in range(mid+1, high+1):
+        sumValue += A[idx]
+        if sumValue > rightSum:
+            rightSum = sumValue
             result.right = idx
 
     result.sumValue = rightSum + leftSum
@@ -52,8 +53,9 @@ def crossMid(A, mid):
     return result
 
 a = [-1, 2, 3, 4, -5]
-pdb.set_trace()
-result = find_maximum_subarray(a)
+#pdb.set_trace()
+result = find_maximum_subarray(a, 0, len(a)-1)
+print('array = ', a)
 print('left Index = ', result.left)
 print('right Index = ', result.right)
 print('Sum = ', result.sumValue)
