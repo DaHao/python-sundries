3f0079d0716965479596c161ede10fe04b5f1867
DaHao <jiash561@gmail.com>
2016-09-12 16:39:40 +0800

    103file --> the order direction is wrong

--- bubleSort.py
+++ bubleSort.py
@@ -6,14 +6,14 @@ def bubleSort(arr):
         idx += 1
             
 def switchVal(arr, valA, valB):
-    i, j = arr.index(valA), arr.index(valB)
+    i, j = arr[valA], arr[valB]
     arr[i], arr[j] = arr[j], arr[i]
     
 def MoveVal(arr, Val):
     idx = arr.index(Val)
     while(idx + 1 < len(arr)):
         if(arr[idx] > arr[idx+1]):
-            switchVal(arr, ValA, ValB)
+            switchVal(arr, idx, idx+1)
             idx += 1
         else:
             break
