ba06a4a294f63c9fd2087d626edc6930845037b6
DaHao <jiash561@gmail.com>
2016-09-12 10:36:04 +0800

    add two sort method

---
+++ bubleSort.py
@@ -0,0 +1,23 @@
+def bubleSort(arr):
+    idx = 0
+    while(idx < len(arr)):
+        keyVal = arr[idx]
+        MoveVal(arr, keyVal)
+        idx += 1
+            
+def switchVal(arr, valA, valB):
+    i, j = arr.index(valA), arr.index(valB)
+    arr[i], arr[j] = arr[j], arr[i]
+    
+def MoveVal(arr, Val):
+    idx = arr.index(Val)
+    while(idx + 1 < len(arr)):
+        if(arr[idx] > arr[idx+1]):
+            switchVal(arr, ValA, ValB)
+            idx += 1
+        else:
+            break
+
+arr = [3, 2, 1]
+bubleSort(arr)
+print(arr)
