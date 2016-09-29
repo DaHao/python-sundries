9c6cd3726ee94947b1066bd6a248c319ffc8eb51
DaHao <jiash561@gmail.com>
2016-09-29 20:52:20 +0800

    Add metaclassSample.py

---
+++ metaclassSample.py
@@ -0,0 +1,22 @@
+#-*- coding=utf-8 -*-
+
+def upper_attr(future_class_name, future_class_parents, future_class_attr):
+    '''return class object, turn attr into UpperCase'''
+    # choose all not begin with '__'
+    attrs = ((name, value) for name, value in future_class_attr.items() if not name.startswith('__'))
+
+    uppercase_attr = dict((name.upper(), value) for name, value in attrs)
+
+    return type(future_class_name, future_class_parents, uppercase_attr)
+
+#__metaclass__ = upper_attr
+
+class Foo(object):
+    __metaclass__ = upper_attr
+    bar = 'bip'
+
+print(hasattr(Foo, 'bar'))
+print(hasattr(Foo, 'BAR'))
+
+f = Foo()
+print(f.BAR)
